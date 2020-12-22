f6527bd49a00bdc956d649112efae03aca7abd5b
Iris <49011122+nhokilli6@users.noreply.github.com>
2020-12-23 00:19:02 +0700

    last update

--- app.js
+++ app.js
@@ -45,12 +45,12 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://reactnative:lehai19031998@cluster0.9s7gi.mongodb.net/mern?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9s7gi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true },
     { useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(5009);
+    app.listen( process.env.PORT || 5009);
     console.log("sad");
   })
   .catch((err) => console.log(err));
